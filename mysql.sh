ab4ed11a2771b41ad455def98d6d375406974796
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-30 00:14:20 +0530

    running script

--- mysql.sh
+++ mysql.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+source ./common.sh
+check_root
+
+dnf install mysql-server -y &>>$log_file
+validate $? "installing mysql-server"
+
+systemctl enable mysqld &>>$log_file
+validate $? "enabled mysql"
+
+systemctl start mysqld &>>$log_file
+validate $? "started mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$log_file
+validate $? "Settingup password for mysql DB as root is default user"
+
+print_time
