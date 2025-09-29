4f9fd3becae32b32c58157300e707e68c929f5c0
bharath-gaveni <bharathgaveni@gmail.com>
2025-09-29 23:32:17 +0530

    running script

--- common.sh
+++ common.sh
@@ -3,8 +3,8 @@ N="\e[0m"
 R="\e[0;31m"
 G="\e[0;32m"
 Y="\e[0;33m"
+id=$(id -u)
 check_root() {
-    id=$(id -u)
     if [ $id -ne 0 ]; then
         echo -e "$R Please execute this script as root user $N"
         exit 1
