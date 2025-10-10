d9aa3905d1460f356a55f5679378715cc25ebc09
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-10 17:23:29 +0530

    shell-roboshop-script

--- frontend.sh
+++ frontend.sh
@@ -4,37 +4,37 @@ source ./common.sh
 check_root
 setup_logging
 
-dnf module disable nginx -y
+dnf module disable nginx -y &>>$log_file
 validate $? "disabling the defaut nginx"
 
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>>$log_file
 validate $? "enabling nginx version 1.24"
 
-dnf install nginx -y
+dnf install nginx -y &>>$log_file
 validate $? "installing nginx"
 
-systemctl enable nginx 
+systemctl enable nginx &>>$log_file
 validate $? "enabling nginx"
 
-systemctl start nginx 
+systemctl start nginx &>>$log_file
 validate $? "start nginx"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>>$log_file
 validate $? "removing the default content in html directory"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$log_file
 validate $? "dowloading the frontend in tmp location"
 
-cd /usr/share/nginx/html 
+cd /usr/share/nginx/html &>>$log_file
 validate $? "changing location to html directory"
 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$log_file
 validate $? "unzipping the frontend code in html dir location"
 
-cp $Dir_name/nginx.conf /etc/nginx/nginx.conf
+cp $Dir_name/nginx.conf /etc/nginx/nginx.conf &>>$log_file
 validate $? "copying nginx.conf"
 
-systemctl restart nginx 
+systemctl restart nginx &>>$log_file
 validate $? "restart nginx"
 
 print_time
